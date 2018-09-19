64bbb704f230fee9bca40978ad4e84e6e508047d
Manan <mpachich@in.ibm.com>
2018-09-19 17:44:20 +0530

    1

--- jenkins.tf
+++ jenkins.tf
@@ -11,55 +11,53 @@ resource "jenkins_job" "first" {
   disabled     = false
 
   parameters = {
-    ProjectUrl = "${var.github_project_url}"
+    ProjectURL = "${var.github_project_url}"
   }
 
   template = <<EOF
-<flow-definition plugin="workflow-job@2.24">
-<actions/>
-<description></description>
-{{- with .Parameters }}
-<keepDependencies>false</keepDependencies>
-<properties>
-<com.coravy.hudson.plugins.github.GithubProjectProperty plugin="github@1.29.2">
-<projectUrl>{{ .ProjectUrl }}</projectUrl>
-<displayName></displayName>
-</com.coravy.hudson.plugins.github.GithubProjectProperty>
-<org.jenkinsci.plugins.workflow.job.properties.PipelineTriggersJobProperty>
-<triggers>
-<hudson.triggers.SCMTrigger>
-<spec>* * * * *</spec>
-<ignorePostCommitHooks>false</ignorePostCommitHooks>
-</hudson.triggers.SCMTrigger>
-</triggers>
-</org.jenkinsci.plugins.workflow.job.properties.PipelineTriggersJobProperty>
-</properties>
-<definition class="org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition" plugin="workflow-cps@2.54">
-<script>node{
-stage ('Code Fetch'){
-checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: 'http://github.com/mpachich/helloWorld.git']]])
+  <flow-definition plugin="workflow-job@2.24">
+  <actions/>
+  <description></description>
+  <keepDependencies>false</keepDependencies>
+  <properties>
+    <com.coravy.hudson.plugins.github.GithubProjectProperty plugin="github@1.29.2">
+      <projectUrl>"http://github.com/mpachich/helloWorld"</projectUrl>
+      <displayName></displayName>
+    </com.coravy.hudson.plugins.github.GithubProjectProperty>
+    <org.jenkinsci.plugins.workflow.job.properties.PipelineTriggersJobProperty>
+      <triggers>
+        <hudson.triggers.SCMTrigger>
+          <spec>* * * * *</spec>
+          <ignorePostCommitHooks>false</ignorePostCommitHooks>
+        </hudson.triggers.SCMTrigger>
+      </triggers>
+    </org.jenkinsci.plugins.workflow.job.properties.PipelineTriggersJobProperty>
+  </properties>
+  <definition class="org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition" plugin="workflow-cps@2.54">
+    <script>node{
+      stage ('Code Fetch'){
+    checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: 'http://github.com/mpachich/helloWorld.git']]])
 
 sh '''if [ -d "helloWorld" ];
-then
-cd helloWorld
-git pull origin master
-else
-git clone http://github.com/mpachich/helloWorld.git
-cd helloWorld
-fi'''
-}
-stage ('Build'){
-sh '''
-javac helloWorld.java
-java helloWorld'''
-}
+  then
+    cd helloWorld
+    git pull origin master
+  else
+    git clone http://github.com/mpachich/helloWorld.git
+      cd helloWorld
+  fi'''
+  }
+  stage ('Build'){
+  sh '''
+    javac helloWorld.java
+    java helloWorld'''
+    }
 }
 </script>
-<sandbox>true</sandbox>
-</definition>
-<triggers/>
-<disabled>false</disabled>
-{{- end }}
+    <sandbox>true</sandbox>
+  </definition>
+  <triggers/>
+  <disabled>false</disabled>
 </flow-definition>
 EOF
 }
