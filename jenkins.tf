1527472d3403e4650cdb2941ca476e75131f8618
Manan <mpachich@in.ibm.com>
2018-09-19 12:50:47 +0530

    1

--- jenkins.tf
+++ jenkins.tf
@@ -1,15 +1,63 @@
 provider "jenkins" {
-  server_url = "http://localhost:8080/"
-  username   = "user1"
-  password   = "user1"
+  server_url = "${var.jenkins_url}"
+  username   = "${var.jenkins_username}"
+  password   = "${var.jenkins_password}"
 }
 
 resource "jenkins_job" "first" {
-  name         = "TerrTest"
-  display_name = "First terraform test"
+  name         = "${var.job_name}"
+  display_name = "${var.display_name}"
   description  = "This makes a project using terraform"
   disabled     = false
 
-  parameters = {}
-  template   = "file://./job_config.xml"
+  parameters = {
+    GithubProjectURL = "${var.github_project_url}"
+  }
+
+  template = <<EOF
+  <flow-definition plugin="workflow-job@2.24">
+  <actions/>
+  <description></description>
+  <keepDependencies>false</keepDependencies>
+  <properties>
+    <com.coravy.hudson.plugins.github.GithubProjectProperty plugin="github@1.29.2">
+      <projectUrl>{{ .GithubProjectURL }}</projectUrl>
+      <displayName></displayName>
+    </com.coravy.hudson.plugins.github.GithubProjectProperty>
+    <org.jenkinsci.plugins.workflow.job.properties.PipelineTriggersJobProperty>
+      <triggers>
+        <hudson.triggers.SCMTrigger>
+          <spec>* * * * *</spec>
+          <ignorePostCommitHooks>false</ignorePostCommitHooks>
+        </hudson.triggers.SCMTrigger>
+      </triggers>
+    </org.jenkinsci.plugins.workflow.job.properties.PipelineTriggersJobProperty>
+  </properties>
+  <definition class="org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition" plugin="workflow-cps@2.54">
+    <script>node{
+      stage ('Code Fetch'){
+    checkout([$class: 'GitSCM', branches: [[name: '*/master']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[url: 'https://github.com/mpachich/helloWorld.git']]])
+
+sh '''if [ -d "helloWorld" ];
+  then
+    cd helloWorld
+    git pull origin master
+  else
+    git clone {{ .GithubProjectURL }}
+      cd helloWorld
+  fi'''
+  }
+  stage ('Build'){
+  sh '''
+    javac helloWorld.java
+    java helloWorld'''
+    }
+}
+</script>
+    <sandbox>true</sandbox>
+  </definition>
+  <triggers/>
+  <disabled>false</disabled>
+</flow-definition>
+EOF
 }
---
+++ var.tf
@@ -0,0 +1,23 @@
+variable "jenkins_url" {
+  description = "Jenkins URL"
+}
+
+variable "jenkins_username" {
+  description = "Jenkins Username"
+}
+
+variable "jenkins_password" {
+  description = "Jenkins Password"
+}
+
+variable "job_name" {
+  description = "Name of Job"
+}
+
+variable "display_name" {
+  description = "Job display name"
+}
+
+variable "github_project_url" {
+  description = "URL of GitHub Project"
+}
