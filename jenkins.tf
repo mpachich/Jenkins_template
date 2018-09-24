5254fb370e36e0727a8e71a4e741076e85d9a411
Manan <mpachich@in.ibm.com>
2018-09-24 14:31:53 +0530

    1

--- jenkins.tf
+++ jenkins.tf
@@ -1,5 +1,5 @@
 provider "jenkins" {
-  server_url = "${var.jenkins_url}"
+  server_url = "${var.jenkins_url}:30001"
   username   = "${var.jenkins_username}"
   password   = "${var.jenkins_password}"
 }
